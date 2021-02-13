c41fc594254647b28eb2e72a75faa93e2e9e8f9c
pedzichl <lukaszpedzich@hotmail.com>
2021-02-13 09:48:41 +0100

    exercise_3

---
+++ guess_the_number_2.py
@@ -0,0 +1,38 @@
+from random import randint
+
+def choices():
+    """
+    acquire decision from user and check if it is correct
+    :rtype: str
+    :return: string from available decisions, chosen by user
+    """
+
+    available_decisions = ["too small", "too big", "you win"]
+    while True:
+        user_choice = input().lower()
+        if user_choice in available_decisions:
+            break
+        else:
+            print("Incorrect decision, insert too small, too big or you win")
+    return user_choice
+
+
+def guess_the_number():
+    print("Guess the number between 1 and 1000 and I'll try to guess it in under 10 tries, unless you cheat!:)")
+    print("Press 'Enter to start")
+    input()
+    min = 0
+    max = 1000
+    user_choice = ""
+    while user_choice != "you win":
+        guess = int((max - min) // 2) + min
+        print(f"Your guess is: {guess}")
+        user_choice = choices()
+        if user_choice == "too small":
+            min = guess
+        elif user_choice == "too big":
+            max = guess
+    print("I've guessed your number!")
+
+if __name__ == "__main__":
+    guess_the_number()
